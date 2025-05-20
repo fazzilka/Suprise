bd3e52a7364171ca5d1b2a9e1264decf8fc3b857
fazzilka <admiral.kanaris3@gmail.com>
2025-05-20 22:30:48 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = 3 + Math.random() * 3 + "s";
+  document.body.appendChild(heart);
+  setTimeout(() => heart.remove(), 6000);
+}
+setInterval(createHeart, 400);
